0e90dbd4f694dd5456cdcc5f8fb87f78f242d82b
jinxtail <148160102+jinxtail@users.noreply.github.com>
2026-09-02 00:43:44 +0000

    02 No Pandas

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,7 +5,8 @@ print('args', sys.argv)
 
 month = int(sys.argv[1])
 
-df = pd.DataFrame({'Day': [1, 2], 'num_passengers': [3, 4]})
+df = pd.DataFrame({'day': [1, 2], 'num_passengers': [3, 4]})
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f'output_{month}.parquet')
